b94f91f1c139f14b6cdeb0d7ea23886ff622a82a
bopad <borisdelord@proton.me>
2025-02-09 02:01:35 +0100

    add fake leetcode svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
